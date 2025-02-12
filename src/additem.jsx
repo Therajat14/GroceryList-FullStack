e3cb9fe2c9564fd99ca25f3c8253265512f397e1
Rajat Singh <heylegend14@gmail.com>
2025-02-13 01:07:56 +0530

    added readme

--- src/additem.jsx
+++ src/additem.jsx
@@ -1,20 +1,29 @@
+import { useRef } from "react";
+import { useReducer } from "react";
 import { FaPlus } from "react-icons/fa";
-const AddItem = ({ newItem, setNewItem, handelSubmit }) => {
 
+
+const AddItem = ({ newItem, setNewItem, handelSubmit }) => {
+    const inputRef = useRef();
     return (
         <form
             className='addItem1'
             onSubmit={handelSubmit}
-            required >
+            required>
+
             <label htmlFor="addItem1" className="addItem">
                 <input
+                    ref={inputRef}
                     type="text"
                     placeholder="Add item"
                     value={newItem}
                     onChange={(e) => setNewItem(e.target.value)}
                     required
                 />
-                <button type="submit"><FaPlus /></button>
+                <button
+                    type="submit"
+                    onClick={() => inputRef.current.focus()}
+                ><FaPlus /></button>
             </label>
         </form>
 
